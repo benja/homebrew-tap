c7786bbb305b8d38e81f5a28d5440e18ab629d88
Benjamin Akar <benjaminakar2001@gmail.com>
2022-09-27 19:48:46 +0200

    feat: update to v0.1.1

--- Formula/cloup.rb
+++ Formula/cloup.rb
@@ -1,10 +1,10 @@
 class Cloup < Formula
+    version "0.1.1"
     desc "Cloup is a template manager that provides the files you desire when you need them, written in Rust."
     homepage "https://github.com/benja/cloup"
-    url "https://github.com/benja/cloup/releases/download/v0.1.0/cloup-0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "be7ff52eecb18b43e6f79b6da1a7a2bce0d51ff46e0a58645eae51aec3848eb7"
-    version "0.1.0"
 
+    url "https://github.com/benja/cloup/releases/download/v#{version}/cloup-#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "8511bad94f99ea441fe2b4473b1bca92a31c31c6093543f76c5b72673772fdde"
 
     def install
         bin.install "cloup"
